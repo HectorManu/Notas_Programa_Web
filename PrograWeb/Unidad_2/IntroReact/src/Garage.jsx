2c8298a3a950ab02485917100e9e2e399f4c2e82
HectorManu <le19081442@itmerida.edu.mx>
2022-11-08 17:49:23 -0600

    agregue cambios

--- PrograWeb/Unidad_2/IntroReact/src/Garage.jsx
+++ PrograWeb/Unidad_2/IntroReact/src/Garage.jsx
@@ -1,14 +1,14 @@
 import Car from './Car'
 
 function Garage() {
-    const autos=[{ marca : "ford", color : "magenta"},{maca : "porch", color : "cyan"},{marca :"Dodge", color :"carmín"}];
+    const unauto=[{ marca : "ford", color : "magenta"},{maca : "porch", color : "cyan"},{marca :"Dodge", color :"carmín"}];
 
     return(
         <div>
             <ul>
-                <li><Car unauto = {autos[0]}></Car></li>
-                <li><Car unauto = {autos[1]}></Car></li>
-                <li><Car unauto = {autos[2]}></Car></li>
+                <li><Car unauto = {unauto[0]}></Car></li>
+                <li><Car unauto = {unauto[1]}></Car></li>
+                <li><Car unauto = {unauto[2]}></Car></li>
             </ul>
         </div>
     );
