2f6759c24903256783a7da57aeea4b10f783b1ef
HectorManu <le19081442@itmerida.edu.mx>
2022-11-15 17:14:46 -0600

    agregue el main y ya vimos el arrow function y también se vio el operador ternario y esta bastante padre

---
+++ PrograWeb/Unidad_2/IntroReact/src/Football.jsx
@@ -0,0 +1,8 @@
+function Football(params) {
+    
+    const patear = (mensaje) => {alert(mensaje)}
+
+    return <button onClick={()=>patear('Hiciste click papu')}>Hacer Click</button>
+}
+
+export default Football;
--- PrograWeb/Unidad_2/IntroReact/src/main.jsx
+++ PrograWeb/Unidad_2/IntroReact/src/main.jsx
@@ -5,13 +5,39 @@ import './index.css'
 
 import Car from './Car'
 import Garage from './Garage'
+import Football from './Football'
+
+function FuePenal() {
+  return <h1>Sí Fue penal</h1>;
+}
+
+function NoFuePenal() {
+  return <h1>No Fue penal</h1>;
+}
+
+function Penal(props) {
+  const fuePenal = props.fuePenal;
+//   if (fuePenal) {
+//     return <FuePenal/>;
+//   }
+//   else return <NoFuePenal/>;
+
+  return fuePenal ? <FuePenal/> : <NoFuePenal/>;
+  
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Car color = "rojo"/> SOLO PONEMOS UN RENDER A LA VEZ
-  <Garage />
-  
+    <Penal fuePenal = {true} />
+    //<Football/>
 );
+
+// const root1 = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <Car color = "rojo"/> SOLO PONEMOS UN RENDER A LA VEZ
+//   <Garage />
+  
+// );
 // PARECE QUE SI PONGO ESTO NO ME FUNCIONA EL COMPONENTE DE ARRIBA 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
